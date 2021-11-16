9975de1ab2e4deafba2bf74a286696699bd29f84
hirose <tak.boc.228@gmail.com>
2021-11-16 06:49:06 +0000

    PHP16課題提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,14 +14,36 @@ class ProfileController extends Controller
     {
      return view('admin.profile.create');
     }
-    public function create()
+    
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile();
+        $form = $request->all();
+        
+       
+        
+        unset($form['_token']);
+     
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
-    public function edit()
+    
+    
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
+    
+    
     public function update()
     {
         return redirect('admin/profile/edit');
