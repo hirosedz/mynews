2f9019314dcc95f05dc838bc450a71f7021a38a2
hirose <tak.boc.228@gmail.com>
2021-11-26 08:20:18 +0000

    Heroku課題提出

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -4,8 +4,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h2>ニュース一覧</h2>
-            <p>投稿されたニュース一覧</p>
+            <h2>投稿されたニュース一覧</h2>
         </div>
         <div class="row">
             <div class="col-md-4">
